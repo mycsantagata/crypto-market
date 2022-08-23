e1d710c5df05ac2ea050209c5dede4525d681253
csantagata <santycmn28@gmail.com>
2022-08-23 16:06:10 +0200

    .

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login/', check_login, name='check_login'),
     path('', check_login, name='check_login'),
-    path('home', home, name='home'),
-    path('new_order', new_order, name='new_order'),
-    path('get_active_orders', get_active_orders, name='get_active_orders')
+    path('login/', check_login, name='check_login'),
+    path('home/', home, name='home'),
+    path('sign_up/', sign_up, name='sign_up'),
+    path('new_order/', new_order, name='new_order'),
+    path('get_active_orders/', get_active_orders, name='get_active_orders')
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
 import json
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
@@ -11,20 +14,65 @@ getCMCData = CMC()
 
 def check_login(request):
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
+        formFields = AuthenticationForm(data=request.POST)
+        if formFields.is_valid():
+            username = formFields.cleaned_data['username']
+            password = formFields.cleaned_data['password']
+            chk_user = authenticate(request, username=username, password=password)
+            login(request, chk_user)
             return redirect('home')
+        else:
+            logout(request)
+            formFields = AuthenticationForm()
+            messages.success(request, "Username or password is incorrect, please try again.")
+            return render(request, 'app/login.html', {'form': formFields})
     else:
-        form = AuthenticationForm()
-        return render(request, 'app/login.html', {'form': form})
+        logout(request)
+        formFields = AuthenticationForm()
+        return render(request, 'app/login.html', {'form': formFields})
 
 
+def sign_up(request):
+    if request.method == "POST":
+        formFields = UserCreationForm(data=request.POST)
+
+        if formFields.is_valid():
+            new_user = formFields.save()
+
+            profile = Profile()
+            profile.user = new_user
+            profile.save()
+
+            username = formFields.cleaned_data.get('username')
+            password = formFields.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('check_login')
+        else:
+            for field, errors in formFields.errors.items():
+                messages.success(request, 'Field: {} Errors: {}'.format(field, errors.as_data()))
+
+            formFields = UserCreationForm()
+            return render(request, 'app/sign_up.html', {'form': formFields})
+    else:
+        form = UserCreationForm()
+        return render(request, 'app/sign_up.html', {'form': form})
+
+
+@login_required(login_url='/login/')
 def home(request):
-    user = request.user
-    profile = get_object_or_404(Profile, user__username=user.username)
-    return render(request, 'app/home.html', {'profile': profile})
+    chk_auth = False
+
+    if request.user.is_authenticated:
+        chk_auth = True
+        user = request.user
+        profile = get_object_or_404(Profile, user__username=user.username)
+        return render(request, 'app/home.html', {'profile': profile, 'chk_auth': chk_auth})
+    else:
+        return render(request, 'app/home.html', {'chk_auth': chk_auth})
 
 
+@login_required(login_url='/login/')
 def new_order(request):
     user = request.user
     profile = get_object_or_404(Profile, user__username=user.username)
@@ -34,48 +82,54 @@ def new_order(request):
         if form.is_valid():
 
             quantity = form.cleaned_data['quantity']
-            type_order = form.cleaned_data['type']
-            order = Order()
-            order.profile = profile
-            order.type = type_order
-            order.quantity = quantity
-            order.price = price
+            if 0 < quantity <= profile.btc:
 
-            if type_order == 2:
-                buy_order = Order.objects.filter(type=1, price__gte=price, quantity=quantity, status=1) \
-                    .order_by('-datetime') \
-                    .order_by('price') \
-                    .first()
+                type_order = form.cleaned_data['type']
+                order = Order()
+                order.profile = profile
+                order.type = type_order
+                order.quantity = quantity
+                order.price = price
 
-                if buy_order is None:
-                    order.status = 1
+                if type_order == 2:
+                    buy_order = Order.objects.filter(type=1, price__gte=price, quantity=quantity, status=1) \
+                        .order_by('-datetime') \
+                        .order_by('price') \
+                        .first()
+
+                    if buy_order is None:
+                        order.status = 1
+                    else:
+                        buy_order.status = 2
+                        buy_order.save()
+                        order.status = 2
+                    order.save()
+
+                    profile.btc -= quantity
+                    profile.save()
+
                 else:
-                    buy_order.status = 2
-                    buy_order.save()
-                    order.status = 2
-                order.save()
+                    sell_order = Order.objects.filter(type=2, price__lt=price, quantity=quantity, status=1) \
+                        .order_by('datetime') \
+                        .order_by('price') \
+                        .first()
 
-                profile.btc -= quantity
-                profile.save()
+                    if sell_order is None:
+                        order.status = 1
+                    else:
+                        sell_order.status = 2
+                        sell_order.save()
+                        order.status = 2
+                    order.save()
 
+                    profile.btc += quantity
+                    profile.save()
+
+                return redirect('home')
             else:
-                sell_order = Order.objects.filter(type=2, price__lt=price, quantity=quantity, status=1) \
-                    .order_by('datetime') \
-                    .order_by('price') \
-                    .first()
-
-                if sell_order is None:
-                    order.status = 1
-                else:
-                    sell_order.status = 2
-                    sell_order.save()
-                    order.status = 2
-                order.save()
-
-                profile.btc += quantity
-                profile.save()
-
-            return redirect('home')
+                messages.success(request, '[Quantity]: invalid value! You do not have enough BTC or you have entered '
+                                          'a value less than 1')
+                return redirect('new_order')
     else:
 
         form = OrderForm()
@@ -84,10 +138,10 @@ def new_order(request):
                                                       'price': price})
 
 
+@login_required(login_url='/login/')
 def get_active_orders(request):
     orders = Order.objects.filter(status=1).order_by('-datetime')
     json_order = {}
-    json_pretty = ''
     buy = []
     sell = []
     for order in orders:
